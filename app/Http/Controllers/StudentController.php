4ae78bfd47f08ee188697cdd13322ad3dea91832
Giuseppe Rotolo <rotolo37@gmail.com>
2020-08-31 17:56:20 +0200

    update controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,27 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
 
 class StudentController extends Controller
 {
   public function index() {
-    $students = [
-      [
-        'nome' => 'Giuseppe',
-        'cognome' => 'Rotolo',
-        'voto' => 9,
-      ],
-      [
-        'nome' => 'Mario',
-        'cognome' => 'Rossi',
-        'voto' => 6,
-      ],
-      [
-        'nome' => 'Ernesto',
-        'cognome' => 'Bassatore',
-        'voto' => 8,
-      ]
-    ];
+    // $students = [
+    //   [
+    //     'nome' => 'Giuseppe',
+    //     'cognome' => 'Rotolo',
+    //     'voto' => 9,
+    //   ],
+    //   [
+    //     'nome' => 'Mario',
+    //     'cognome' => 'Rossi',
+    //     'voto' => 6,
+    //   ],
+    //   [
+    //     'nome' => 'Ernesto',
+    //     'cognome' => 'Bassatore',
+    //     'voto' => 8,
+    //   ]
+    // ];
+
+    $students = Student::all();
+
 
     return view('students', compact('students'));
   }
